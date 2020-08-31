128d96d0083b4a2496d6e2364ab7b146070ca246
Karim Alomari <karim.alomari@gmail.com>
2020-08-31 16:24:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
+  attr_reader :discount, :items
+  attr_accessor :total
 
+  def initialize (discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end
+  
+end
